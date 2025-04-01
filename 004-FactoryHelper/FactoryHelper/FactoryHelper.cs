1f5082ac4501ce520d9d33b8ef9471d969ff9755
rfondacci <riccardo.fondacci@quixant.com>
2025-04-01 09:47:42 +0200

    factory helper class tentative.

---
+++ 004-FactoryHelper/FactoryHelper/FactoryHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata;
+
+namespace FactoryHelper
+{
+    public static class FactoryHelper
+    {
+        public static /*nint*/object? Factory(string assemblyName, string typeName, string methodName, int ctorParams, params object[] args)
+        {
+            Assembly assembly = Assembly.Load(assemblyName);
+            object? instance = assembly.CreateInstance(typeName,false, BindingFlags.Default, null, args[..(ctorParams-1)], null, null);
+            object? res = assembly.GetType(typeName).GetMethod(methodName).Invoke(instance, args[ctorParams..]);
+            //return (nint)res;
+            return res;
+        }
+    }
+}
